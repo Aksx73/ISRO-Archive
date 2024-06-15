587f1bfee3c38096b272099c6e3ad6e492d24137
Akshay <akshaynighot73@gmail.com>
2024-06-15 21:10:24 +0530

    homescreen view in compose

--- app/src/main/java/com/absut/isro/archive/ui/HomeFragment.kt
+++ app/src/main/java/com/absut/isro/archive/ui/HomeFragment.kt
@@ -70,6 +70,100 @@ class HomeFragment : Fragment() {
             }
         }
 
+        return binding.root*/
+
+
+        return ComposeView(requireContext()).apply {
+            // Dispose of the Composition when the view's LifecycleOwner is destroyed
+            setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+            setContent {
+                MaterialTheme {
+                    HomeScreen()
+                }
+            }
+
+        }
+
+
+    }
+
+    @Composable
+    private fun HomeScreen(modifier: Modifier = Modifier) {
+        Column(modifier = modifier
+            .padding(16.dp)
+            .verticalScroll(rememberScrollState())) {
+            ContentView(
+                title = R.string.spacecraft_label,
+                desc = R.string.spacecraft_desc,
+                image = R.drawable.ic_rocket_black_24dp
+            )
+            Spacer(modifier = Modifier.height(12.dp))
+            ContentView(
+                title = R.string.launchers_label,
+                desc = R.string.launchers_desc,
+                image = R.drawable.ic_rocket_launch_black_24dp
+            )
+            Spacer(modifier = Modifier.height(12.dp))
+            ContentView(
+                title = R.string.customer_satellite_label,
+                desc = R.string.customer_satellite_desc,
+                image = R.drawable.ic_satellite_black_24dp
+            )
+            Spacer(modifier = Modifier.height(12.dp))
+            ContentView(
+                title = R.string.centres_label,
+                desc = R.string.centres_desc,
+                image = R.drawable.ic_hub_black_24dp
+            )
+        }
+
+    }
+
+    @Composable
+    private fun ContentView(
+        modifier: Modifier = Modifier,
+        @StringRes title: Int,
+        @StringRes desc: Int,
+        @DrawableRes image: Int,
+        clickListener: () -> Unit = {}
+    ) {
+        Surface(
+            shape = Shapes().small,
+            border = BorderStroke(1.dp, MaterialTheme.colorScheme.outline),
+            color = MaterialTheme.colorScheme.surface,
+            onClick = clickListener
+        ) {
+            Row(
+                modifier = Modifier.padding(16.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Column(
+                    modifier = Modifier
+                        .weight(1f)
+                        .padding(end = 8.dp)
+                ) {
+                    Text(
+                        text = stringResource(title),
+                        style = MaterialTheme.typography.headlineSmall,
+                        fontFamily = FontFamily(Font(R.font.roboto_mono_medium))
+                    )
+                    Text(
+                        text = stringResource(desc),
+                        modifier = Modifier.padding(top = 8.dp),
+                        maxLines = 3,
+                        fontFamily = FontFamily(Font(R.font.roboto_mono_regular)),
+                        fontWeight = FontWeight.Normal
+                    )
+                }
+                Image(
+                    painter = painterResource(image),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .width(90.dp)
+                        .height(90.dp)
+                )
+            }
+        }
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
