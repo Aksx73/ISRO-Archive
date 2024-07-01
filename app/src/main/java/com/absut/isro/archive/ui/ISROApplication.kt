de4323b2c06088384c63f1bcf0d6b196cda2c221
Akshay <akshaynighot73@gmail.com>
2024-07-01 08:00:30 +0530

    dynamic color support enabled for xml based layout

--- app/src/main/java/com/absut/isro/archive/ui/ISROApplication.kt
+++ app/src/main/java/com/absut/isro/archive/ui/ISROApplication.kt
@@ -1,7 +1,14 @@
 package com.absut.isro.archive.ui
 
 import android.app.Application
+import com.google.android.material.color.DynamicColors
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class ISROApplication: Application()
+class ISROApplication: Application(){
+
+    override fun onCreate() {
+        super.onCreate()
+        DynamicColors.applyToActivitiesIfAvailable(this)
+    }
+}
