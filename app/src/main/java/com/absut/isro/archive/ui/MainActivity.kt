f3f9baaaf160f4106b143220de814be1184220fc
Akshay <akshaynighot73@gmail.com>
2024-09-07 22:43:07 +0530

    add navController parameter for all entry point screens

--- app/src/main/java/com/absut/isro/archive/ui/MainActivity.kt
+++ app/src/main/java/com/absut/isro/archive/ui/MainActivity.kt
@@ -56,7 +56,11 @@ class MainActivity : AppCompatActivity() {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         return when (item.itemId) {
-            R.id.action_about -> true
+            R.id.action_about -> {
+                //do something
+                true
+            }
+
             else -> super.onOptionsItemSelected(item)
         }
     }
