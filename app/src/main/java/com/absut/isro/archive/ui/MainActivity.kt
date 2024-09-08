9ccb4cb1b3edb4ea1a3e9ed25d6b76eafed3a65b
Akshay <akshaynighot73@gmail.com>
2024-09-08 19:41:22 +0530

    updated main screen with composable

--- app/src/main/java/com/absut/isro/archive/ui/MainActivity.kt
+++ app/src/main/java/com/absut/isro/archive/ui/MainActivity.kt
@@ -23,25 +23,105 @@ class MainActivity : AppCompatActivity() {
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
 
+    private val viewModel: ISROViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         WindowCompat.setDecorFitsSystemWindows(window, false)
         super.onCreate(savedInstanceState)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        /* binding = ActivityMainBinding.inflate(layoutInflater)
+         setContentView(binding.root)
 
-        setSupportActionBar(binding.toolbar)
+         setSupportActionBar(binding.toolbar)
 
-        val navHostFragment =
-            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        val navController = navHostFragment.navController
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
+         val navHostFragment =
+             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+         val navController = navHostFragment.navController
+         appBarConfiguration = AppBarConfiguration(navController.graph)
+         setupActionBarWithNavController(navController, appBarConfiguration)*/
+
+        setContent {
+            AppTheme {
+                MainScreen()
+            }
+        }
 
     }
 
+    @OptIn(ExperimentalMaterial3Api::class)
     @Composable
-    fun MainScreen(modifier: Modifier = Modifier) {
+    fun MainScreen() {
+        val navController = rememberNavController()
+        var showMenu by remember { mutableStateOf(false) }
+
+        Scaffold(
+            topBar = {
+                TopAppBar(title = {
+                    Text(
+                        text = stringResource(id = R.string.app_name),
+                        modifier = Modifier.fillMaxWidth(),
+                        textAlign = TextAlign.Center
+                    )
+                }, actions = {
+                    IconButton(onClick = { showMenu = !showMenu }) {
+                        Icon(Icons.Default.MoreVert, contentDescription = "More")
+                    }
+                    DropdownMenu(
+                        expanded = showMenu,
+                        onDismissRequest = { showMenu = false }
+                    ) {
+                        DropdownMenuItem(
+                            onClick = { /* Handle settings click */ },
+                            text = { Text(text = "About") }
+                        )
+                    }
+                },
+                    navigationIcon = {
+                        //  if (navController.previousBackStackEntry != null) {
+                        IconButton(onClick = { navController.navigateUp() }) {
+                            Icon(
+                                imageVector = Icons.AutoMirrored.Outlined.ArrowBack,
+                                contentDescription = "Back navigation"
+                            )
+                        }
+                        // }
+                    }
+                )
+            }
+        ) { innerPadding ->
+            NavHost(
+                navController = navController,
+                startDestination = "home",
+                modifier = Modifier.padding(innerPadding)
+            ) {
+                composable("home") { HomeScreen(navController = navController) }
+                composable("spacecraft") {
+                    SpacecraftScreen(
+                        navController = navController,
+                        viewModel = viewModel
+                    )
+                }
+                composable("satellite") {
+                    SatelliteScreen(
+                        navController = navController,
+                        viewModel = viewModel
+                    )
+                }
+                composable("launchers") {
+                    LauncherScreen(
+                        navController = navController,
+                        viewModel = viewModel
+                    )
+                }
+                composable("center") {
+                    CentersScreen(
+                        navController = navController,
+                        viewModel = viewModel
+                    )
+                }
+            }
+
+        }
 
     }
 
