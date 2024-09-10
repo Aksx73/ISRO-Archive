c5e9ba9c164e212bf42aecde19909ad5d60517a9
Akshay <akshaynighot73@gmail.com>
2024-09-10 08:55:06 +0530

    Added lift on scroll feature to topAppBar

--- app/src/main/java/com/absut/isro/archive/ui/MainActivity.kt
+++ app/src/main/java/com/absut/isro/archive/ui/MainActivity.kt
@@ -3,11 +3,41 @@ package com.absut.isro.archive.ui
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.outlined.ArrowBack
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material.icons.outlined.ArrowBack
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.view.WindowCompat
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
@@ -15,6 +45,12 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import com.absut.isro.archive.R
 import com.absut.isro.archive.databinding.ActivityMainBinding
+import com.absut.isro.archive.ui.screens.CentersScreen
+import com.absut.isro.archive.ui.screens.HomeScreen
+import com.absut.isro.archive.ui.screens.LauncherScreen
+import com.absut.isro.archive.ui.screens.SatelliteScreen
+import com.absut.isro.archive.ui.screens.SpacecraftScreen
+import com.example.compose.AppTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -55,8 +91,10 @@ class MainActivity : AppCompatActivity() {
         var showMenu by remember { mutableStateOf(false) }
         val currentBackStackEntry by navController.currentBackStackEntryAsState()
         val currentDestination = currentBackStackEntry?.destination
+        val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior()
 
         Scaffold(
+            modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
             topBar = {
                 TopAppBar(title = {
                     Text(
@@ -90,7 +128,8 @@ class MainActivity : AppCompatActivity() {
                                 )
                             }
                         }
-                    }
+                    },
+                    scrollBehavior = scrollBehavior
                 )
             }
         ) { innerPadding ->
@@ -128,6 +167,14 @@ class MainActivity : AppCompatActivity() {
 
         }
 
+    }
+
+    @Preview
+    @Composable
+    private fun MainScreenPreview() {
+        AppTheme {
+            MainScreen()
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
